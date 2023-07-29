ca078ab3023437826c8c9df087bd716ef1e2cff4
janrae.fagaragan <janrae.fagaragan@student.passerellesnumeriques.org>
2023-07-29 10:28:59 +0800

    push

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
