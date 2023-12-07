5e6f862914fde50ff57a5e2118a2a39d8bb58317
janrae.fagaragan <janrae.fagaragan@student.passerellesnumeriques.org>
2023-12-07 10:33:38 +0800

    register blade deleted

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,8 +27,9 @@
                         <div class="input-field">
                             <i class="fas fa-user"></i>
 
-                            <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" placeholder="Email" type="email"
+                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -41,7 +42,6 @@
                             <input id="password" type="password" placeholder="Password"
                                 class="form-control @error('password') is-invalid @enderror" name="password" required
                                 autocomplete="current-password">
-
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -49,122 +49,32 @@
                             @enderror
                         </div>
                         <input type="submit" value="Login" class="btn solid" />
-
-                        <div class="forgetpassword">
-                            @if (Route::has('password.request'))
-                                <a href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
-
                     </form>
 
+                </div>
 
+                <div class="panels-container">
+                    <div class="panel right-panel">
 
-
-
-
-
-                    {{-- <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <h2 class="title">Sign up</h2>
-                        <div class="input-field">
-                            <i class="fas fa-user"></i>
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                placeholder="Username" name="name" value="{{ old('name') }}" required
-                                autocomplete="name" autofocus>
-                            @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="content">
+                            <h3>One of us ?</h3>
+                            <p>
+                                ADOPISOFT
+                                The leading software provider for piso wifi vending machines. Founded in 2017, it has
+                                completely
+                                revolutionized the piso wifi industry. Currently, there are over 140,000(and still growing)
+                                active machines powered by Adopisoft.
+                            </p>
+                            <button class="btn transparent" id="sign-in-btn">
+                                Sign in
+                            </button>
                         </div>
-
-                        <div class="input-field">
-                            <i class="fas fa-envelope"></i>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                name="email" value="{{ old('email') }}" required autocomplete="email"
-                                placeholder="Email">
-
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-
-                        </div>
-                        <div class="input-field">
-                            <i class="fas fa-lock"></i>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password"  placeholder="Password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-
-                        <div class="input-field">
-                            <i class="fas fa-lock"></i>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Register') }}
-                        </button>
- --}}
-
-
-                        {{-- <input type="submit" class="btn" value="Sign up" /> --}}
-                    </form>
-
-
-
-
-
-
-
-
-
+                        <img src="" class="image" alt="" />
+                    </div>
                 </div>
             </div>
 
-            <div class="panels-container">
-                <div class="panel left-panel">
-                    <div class="content">
-                        <h2>Have no Account ?</h2>
-                        <p>
-                            Innovations driven by passion to bring positive impact to people's lives.
-                        </p>
-                        <button class="btn transparent" id="sign-up-btn">
-                            Sign up
-                        </button>
-                    </div>
-                    <img src="" class="image" alt="" />
-                </div>
-                <div class="panel right-panel">
-
-                    <div class="content">
-                        <h3>One of us ?</h3>
-                        <p>
-                            ADOPISOFT
-                            The leading software provider for piso wifi vending machines. Founded in 2017, it has
-                            completely
-                            revolutionized the piso wifi industry. Currently, there are over 140,000(and still growing)
-                            active machines powered by Adopisoft.
-                        </p>
-                        <button class="btn transparent" id="sign-in-btn">
-                            Sign in
-                        </button>
-                    </div>
-                    <img src="" class="image" alt="" />
-                </div>
-            </div>
-        </div>
-
-        <script src="{{ asset('js/login.js') }}"></script>
+            <script src="{{ asset('js/login.js') }}"></script>
 
     </body>
 
